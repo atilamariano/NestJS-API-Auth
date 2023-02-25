0f70b05a62ed83ce683937298eef12b68542413d
Atila Mariano <103537702+atilamariano@users.noreply.github.com>
2023-02-25 11:51:59 -0300

    feat: Login authentication with bcrypt

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { UnauthorizedError } from './errors/unauthorized.error';
 
 @Injectable()
 export class AuthService {
-  validateUser(email: string, password: string) {
-    throw new Error('Method not implemented.');
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      // checar se a senha informada correponde a hash que esta no bd
+
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+
+    // tratamento de erro de senha ou e-mail
+    throw new UnauthorizedError(
+      'E-mail address or password provided is incorrect.',
+    );
   }
 }
